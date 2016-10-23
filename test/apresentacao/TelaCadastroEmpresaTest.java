3c6ca0ed26e88c053425366df81818d249f4da03
Matheus <Matheus@192.168.1.37>
2016-10-23 18:57:12 -0200

    Classes de teste adicionadas.

---
+++ test/apresentacao/TelaCadastroEmpresaTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apresentacao;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Matheus
+ */
+public class TelaCadastroEmpresaTest {
+    
+    public TelaCadastroEmpresaTest() {
+    }
+    
+}
